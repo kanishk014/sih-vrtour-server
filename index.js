81f2aa9bdad9c8b029738dcfc5eb63f54f69e9b2
kanishk <kanishk.9871@gmail.com>
2022-03-29 12:57:12 +0530

    Updates

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(
 	})
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 // app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV == "development") {
