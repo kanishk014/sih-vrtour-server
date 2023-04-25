30633786107b1e07dffccbc7b90d3528179a92e8
SushantPatial <sushantpatial18@gmail.com>
2023-04-25 14:05:14 +0530

    AW

--- utils/s3.js
+++ utils/s3.js
@@ -1,10 +1,10 @@
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
 
-const bucketName=process.env.AWS_BUCKET_NAME
-const bucketRegion=process.env.AWS_BUCKET_REGION
-const accessKeyId=process.env.AWS_ACCESS_KEY
-const secretAccessKey=process.env.AWS_SECRET_KEY
+const bucketName=process.env.AW_BUCKET_NAME
+const bucketRegion=process.env.AW_BUCKET_REGION
+const accessKeyId=process.env.AW_ACCESS_KEY
+const secretAccessKey=process.env.AW_SECRET_KEY
 
 const s3 = new S3({
   bucketRegion,
